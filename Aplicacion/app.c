4e1222f1d4672098aaa21cf1ef4f4c68cb9c8df4
fgomez98 <33160311+fgomez98@users.noreply.github.com>
2018-08-16 13:24:17 -0300

    Add files via upload

---
+++ Aplicacion/app.c
@@ -0,0 +1,165 @@
+//
+//  main.c
+//  Aplicacion
+//
+//  Created by Fermin Gomez on 8/13/18.
+//  Copyright © 2018 Fermin Gomez. All rights reserved.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/shm.h>
+#include <assert.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <string.h>
+#define SLAVES 5
+#define BUFFER_SIZE 1
+
+void * mapSharedMemory(int id);
+int allocateSharedMemory(int n);
+//void actionHandler (int signum);
+void actionHandler(int signo, siginfo_t *info, void *context);
+void writeDataToBuffer();
+void pipeSlaves(int * fd);
+
+
+const  char * semName = "semaforo";
+sem_t * sem;
+int fdHash[2*SLAVES]; // hash
+int fdFiles[2*SLAVES]; // archivos
+char * shmAddr; // sheared memory adress (buffer)
+
+int main(int argc, const char * argv[]) {
+    pid_t childs[SLAVES];
+    int filesAmount = argc - 1;
+    
+    if (filesAmount == 0) {
+        // error no hay archivos
+    }
+    
+    pipeSlaves(fdHash);
+    pipeSlaves(fdFiles);
+
+    // genero esclavos
+    for (int i = 0; i < SLAVES; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // cierro extremos del pipe que el hijo no va a utilizar
+            
+            close(fdFiles[2*i+1]); // escritura
+            close(fdHash[2*i]); // lectura
+            char * args[]= {}; //argumentos para el esclavo
+            execv("./Esclavo", args); // llamada al proceso esclavo
+        } else {
+            childs[i] = pid;
+            // cierro extremos del pipe que el padere no va a utilizar
+            close(fdFiles[2*i]); // lectura
+            close(fdHash[2*i+1]); // escritura
+        }
+    }
+    // creamos memoria compratida
+    int shmId = allocateSharedMemory(BUFFER_SIZE);
+    if (shmId == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    
+    // mapeamos memoria compartida
+    shmAddr = (char *) mapSharedMemory(shmId);
+    if (!shmAddr) {
+        perror("shmat");
+        exit(1);
+    }
+    
+    // el semaforo se inicializa en 0
+    sem = sem_open(semName, O_CREAT|O_EXCL , S_IRUSR| S_IWUSR , 0);
+    
+    // set up de senales
+    struct sigaction action;
+    //action.sa_handler = &actionHandler;
+    sigemptyset(&action.sa_mask);
+    //action.sa_flags = 0;
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = &actionHandler;
+    sigaction(SIGUSR1, &action, NULL); // mapeo senal de finalizacion de tarea de hash
+    
+    int initialDistribution = filesAmount*0.4;
+    int j = 0;
+    for (int i = 1; i < initialDistribution; i++) {
+        write(fdHash[2*j], argv[i], strlen(argv[i])+1); // +1 para que ponga el null
+        j = (j + 1) % SLAVES;
+    }
+    
+    // cerramos el semaforo
+    sem_close(sem);
+
+    return 0;
+}
+
+void * mapSharedMemory(int id) {
+    void * addr;
+    assert(id != 0);
+    addr = shmat(id, NULL, 0);
+    // shmctl(id, IPC_RMID, NULL); NOSE QUE ES ESTO
+    return addr;
+}
+
+int allocateSharedMemory(int n) {
+    assert(n > 0);
+    return shmget(IPC_PRIVATE, n, IPC_CREAT | SHM_R | SHM_W); // averiguar que es IPC_PRIVATE
+}
+
+//void actionHandler (int signum) {
+//    switch (signum) {
+//        case SIGUSR1:
+//            sem_wait(sem);
+//            writeDataToBuffer();
+//            sem_post(sem);
+//            break;
+//        default:
+//            break;
+//    }
+//
+//}
+
+void actionHandler(int signum, siginfo_t * info, void * context) {
+    switch (signum) {
+        case SIGUSR1: // senal de que el escalvo termino de realizar su tarea
+            sem_wait(sem);
+            writeDataToBuffer();
+            // mandamos un archivo a procesar al esclavo con info->si_pid (pid del escalvo que finalizo)
+            sem_post(sem);
+            break;
+        default:
+            break;
+    }
+}
+
+void writeDataToBuffer() {
+
+}
+
+void pipeSlaves(int * fd) {
+    for (int i = 0; i < SLAVES; i++) {
+        pipe(&fd[2*i]);
+    }
+}
+
+
+//struct sigaction {
+//    void       (*sa_handler)(int);
+//    sigset_t   sa_mask;
+//    int        sa_flags;
+//    void       (*sa_sigaction)(int, siginfo_t *, void *);
+//};
+
+
+
+
+
+

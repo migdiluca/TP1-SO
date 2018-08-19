96f73d099bfc0318edd3e7cf9adc7283be3bcb99
Pedro Vedoya <33105601+pvedoya@users.noreply.github.com>
2018-08-19 12:08:24 -0300

    Add files via upload
    funciona

--- slave.c
+++ slave.c
@@ -10,61 +10,48 @@
 #include <stdio.h>
 
 #define MD5_LENGTH 32
-#define PATH_LENGTH 256
+#define BUFFER_LENGTH 100
+char buffer[BUFFER_LENGTH];
 
+int nextInBuffer(char * src, char * ans);
 
 int main(){
 
-  char aux[PATH_LENGTH];
   char * md5 = NULL; //Stores the hash
 
-  int sret;
-  fd_set readfds;
-  struct timeval timeout;
-
-
   while( 1 ){
-    FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
+    int bytesRead = read( STDIN_FILENO, buffer, BUFFER_LENGTH);
+    int i = 0;
 
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
+    while(i < bytesRead){
+      char * path = malloc(bytesRead - i);
+      i += nextInBuffer(buffer+i, path);
 
-    sret = select(8,&readfds,NULL,NULL,&timeout);
+      char aux[8 + BUFFER_LENGTH] = "md5sum ";
+      strcat(aux,path);
+      md5=malloc(MD5_LENGTH);
 
-    if(sret == 0){
-
-      printf("Theres a slave without any tasks");
-
-    }else{
-
-      if(read( STDIN_FILENO, aux, sizeof(char)) > 0 /*&& aux != NULL*/){
-
-        md5=malloc(MD5_LENGTH + 8);
-        strcat(md5,"md5sum ");
-
-        FILE * file = popen(aux,"r");
-
-        int i,ch;
-
-        for(i = 0; i < MD5_LENGTH && isxdigit(ch = fgetc(file+i));i++){ //Taken from Stack Overflow
-          md5[i+8] = ch;
-        }
-
-        md5[MD5_LENGTH+7]='\0';
-
-        pclose(file);
-
-        write( STDOUT_FILENO, md5, sizeof(char) > 0);
-
-        free(md5);
-
-      }else{
-        break ;
+      FILE * file = popen(aux,"r");
+      int j,ch;
+      for(j = 0; j < MD5_LENGTH && isxdigit(ch = fgetc(file));j++){ //Taken from Stack Overflow
+        md5[j] = ch;
       }
-      //deberia esperar a que le den el siguiente aca
+      strcat(md5, "\0");
+      pclose(file);
+      write( STDOUT_FILENO, md5, BUFFER_LENGTH);
+      free(md5);
     }
-    }
+  }
 
   return 0;
+}
+
+int nextInBuffer(char * src, char * ans) {
+    int i = 0;
+    while (*(src+i) != '\0') {
+        *(ans+i) = *(src+i);
+        i++;
+    }
+    *(ans+i) = '\0';
+    return ++i;
 }

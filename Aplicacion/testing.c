1c680722ec1957e89ccfb16e5601b15f88acf8d0
Pedro Vedoya <33105601+pvedoya@users.noreply.github.com>
2018-08-22 18:35:10 -0300

    Add files via upload
    funcion de testear

---
+++ Aplicacion/testing.c
@@ -0,0 +1,73 @@
+//testing.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PATH_LENGTH 100
+#define MD5_LENGTH 32
+
+int main(){
+  FILE * testFile;
+  FILE * file;
+  char reader[PATH_LENGTH];
+  int i,j=0;
+  char c;
+
+  char aux[8+PATH_LENGTH];
+
+  int md5 = 0;
+  int counter;
+
+  testFile = fopen("md5Hashes.txt", "r");
+  char buff[33];
+
+
+  while((c = fgetc(testFile)) != EOF){
+    switch(c){
+      case '<':
+              if(!md5){
+                strcpy(aux, "md5sum ") ;
+                counter = 0;
+              }
+              break;
+      case '>' :
+                  if(!md5){
+                    aux[counter + 7] = '\0';
+                    printf("Calculando %s\n", aux);
+                    file = popen(aux, "r");
+                    int ch;
+                    for(j = 0; j < MD5_LENGTH && isxdigit(ch = fgetc(file));j++){
+                      buff[j] = ch;
+                    }
+                    buff [j] = '\0';
+                    j=0;
+                    printf("El md5 es:%s\n", buff);
+                    pclose(file);
+                    md5 = 1;
+                  }else{
+                    printf("%s\n", "Coinciden");
+                    md5 = 0;
+                  }
+                  counter = 0;
+                  break;
+      case ' ':   break;
+      case '\n':  break;
+      case ':' :  break;
+      default :
+              if(!md5){
+                aux[7 + counter] = c;
+                counter++;
+              }else{
+                // printf("%c - %c\n",buff[j], c );
+                if(buff[j++] != c){
+                  printf("Error con el calculo de: %s\n", aux);
+                  return 1;
+                }
+              }
+              break;
+    }
+  }
+  fclose(testFile);
+  printf("No se han encontrado problemas con los hashes de los archivos\n");
+  return 0;
+}
